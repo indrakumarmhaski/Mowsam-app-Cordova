68e4ba9d374a9caf3198ccbae9fc6dcd5bd281dd
indrakumarmhaski <indrakumarmhaski@gamil.com>
2018-07-12 17:41:46 +0530

    Created the project

---
+++ platforms/android/app/src/main/assets/www/js/wheater.js
@@ -0,0 +1,40 @@
+﻿var OpenWeatherAppKey = "682219f7b7874eea17c05774dec89263";
+
+function getWeatherWithZipCode() {
+    var zipcode = $('#zip-code-input').val();
+    var queryString =
+        'https://api.openweathermap.org/data/2.5/weather?zip='
+        + zipcode + ',IN&appid=' + OpenWeatherAppKey + '&units=metric';
+    $.getJSON(queryString, function (results) {
+        showWeatherData(results);
+    }).fail(function (jqXHR) {
+        $('#error-msg').show();
+        $('#error-msg').text("Error retrieving data. " + jqXHR.statusText);
+    });
+    return false;
+}
+
+function showWeatherData(results) {
+
+    if (results.weather.length) {
+        $('#error-msg').hide();
+        $('#weather-data').show();
+
+        $('#title').text(results.name);
+        $('#temperature').text(results.main.temp);
+        $('#wind').text(results.wind.speed);
+        $('#humidity').text(results.main.humidity);
+        $('#visibility').text(results.weather[0].main);
+
+        var sunriseDate = new Date(results.sys.sunrise * 1000);
+        $('#sunrise').text(sunriseDate.toLocaleTimeString());
+
+        var sunsetDate = new Date(results.sys.sunset * 1000);
+        $('#sunset').text(sunsetDate.toLocaleTimeString());
+
+    } else {
+        $('#weather-data').hide();
+        $('#error-msg').show();
+        $('#error-msg').text("Error retrieving data. ");
+    }
+}
